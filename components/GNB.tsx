ce0d221f20563dc7a469ae431476f74bcfb6d9f8
scripto1 <heeyongi@gmail.com>
2024-11-29 12:25:27 +0900

    feat: 모바일메뉴추가

--- components/GNB.tsx
+++ components/GNB.tsx
@@ -1,28 +1,64 @@
 import Image from "next/image";
 import SearchComponent from "./SearchComponent";
 import { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 export default function GNB({ searchInput, setSearchInput }) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="flex h-16 px-10 items-center justify-between border-gray-700 border-b backdrop-blur-lg">
-      <Image
-        src="/images/noteit_logo_white.svg"
-        alt="logo"
-        width={110}
-        height={50}
-      />
-      <section className="flex items-center gap-3">
-        <SearchComponent
-          searchInput={searchInput}
-          setSearchInput={setSearchInput}
+    <>
+      <nav className="flex h-16 px-10 items-center justify-between border-gray-700 border-b backdrop-blur-lg">
+        <Image
+          src="/images/noteit_logo_white.svg"
+          alt="logo"
+          width={110}
+          height={50}
         />
-        <button className="h-8 px-5 pt-1 font-semibold text-gray-300 text-base border border-amber-400 hover:bg-gray-800 transition-all duration-200 ease-in-out rounded-3xl">
-          Login
-        </button>
-        <button className="h-8 px-5 pt-1 font-semibold text-gray-800 text-base bg-amber-300 hover:bg-amber-500 transition-all duration-200 ease-in-out rounded-3xl">
-          Sign Up
-        </button>
-      </section>
-    </nav>
+        <section className="flex items-center gap-3">
+          <SearchComponent
+            searchInput={searchInput}
+            setSearchInput={setSearchInput}
+          />
+          <button className="h-8 px-5 pt-1 font-semibold text-gray-300 text-base border border-amber-400 hover:bg-gray-800 transition-all duration-200 ease-in-out rounded-3xl mobile:hidden">
+            Login
+          </button>
+          <button className="h-8 px-5 pt-1 font-semibold text-gray-800 text-base bg-amber-300 hover:bg-amber-500 transition-all duration-200 ease-in-out rounded-3xl mobile:hidden">
+            Sign Up
+          </button>
+          {isMenuOpen ? (
+            <X
+              color="#fff"
+              className="desktop:hidden tablet:hidden cursor-pointer"
+              onClick={handleToggleMenu}
+            />
+          ) : (
+            <Menu
+              color="#fff"
+              className="desktop:hidden tablet:hidden cursor-pointer"
+              onClick={handleToggleMenu}
+            />
+          )}
+        </section>
+      </nav>
+      {isMenuOpen ? (
+        <menu className="flex flex-col p-5 desktop:hidden tablet:hidden border-gray-700 border-b gap-2 transition-transform duration-300 ease-in-out animate-fadeInDown">
+          <li className="flex h-8 items-center hover:bg-gray-800 rounded-md cursor-pointer">
+            <button className="pl-3 font-bold text-gray-300 text-lg ">
+              Login
+            </button>
+          </li>
+          <li className="flex h-8 items-center hover:bg-gray-800 rounded-md cursor-pointer">
+            <button className="pl-3 font-bold text-gray-300 text-lg ">
+              Sign Up
+            </button>
+          </li>
+        </menu>
+      ) : null}
+    </>
   );
 }
